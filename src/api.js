347566775a825f1eafdea71a570191ebfacadbf0
Peter Frohner <peter.frohner@backmarket.com>
2019-09-20 23:49:17 +0200

    remove cl

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 export function fetchRandomActivity() {
-    console.log('aa')
     // TODO: make sure 'fetch' is supported (looking at you, IE), otherwise use axios instead.
     return fetch(
         `http://www.boredapi.com/api/activity/`
